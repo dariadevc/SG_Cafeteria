d91e55f1df570f6a340c429fac999f10ae42dad8
Daria Sauco <dsauco36@gmail.com>
2024-05-03 10:33:15 -0300

    03-05-24 [Método de Pago DAO]

---
+++ Modelo/metodo_pago_DAO.py
@@ -0,0 +1,19 @@
+from base_de_datos import BaseDeDatos
+
+class MetodoPagoDAO:
+    def __init__(self):
+        self.__base = BaseDeDatos()
+
+    def habilitar_metodo(self):
+
+
+    def deshabilitar_metodo(self):
+
+
+    def obtener_un_metodo(self):
+
+
+    def obtener_todos_metodos(self):
+
+
+    def modificar_descuento(self):
