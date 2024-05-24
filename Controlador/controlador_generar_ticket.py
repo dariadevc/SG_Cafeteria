871b0ced2589e67b44a2db0e6884347a30823d2d
Daria Sauco <126127245+dariadevc@users.noreply.github.com>
2024-05-24 13:05:16 -0300

    24-05-24 [Controlador ticket]

--- Controlador/controlador_generar_ticket.py
+++ Controlador/controlador_generar_ticket.py
@@ -26,25 +26,19 @@ class ControladorTicket:
                 self.__vista_ticket.tabla1.setItem(fila_seleccionada, 2, QTableWidgetItem(str(cantidad_disponible - 1)))
         
     def restar_producto(self):
-        def restar_producto(self):
-        # Obtener el producto seleccionado de la tabla 2
         items_seleccionados = self.__vista_ticket.tabla2.selectedItems()
         if len(items_seleccionados) > 0:
-            fila_seleccionada = items_seleccionados[0].row()
+            # fila_seleccionada = items_seleccionados[0].row()
             producto = items_seleccionados[0].text()
 
             for fila in range(self.__vista_ticket.tabla2.rowCount()):
                 if self.__vista_ticket.tabla2.item(fila, 0).text() == producto:
                     cantidad_actual = int(self.__vista_ticket.tabla2.item(fila, 1).text())
-                    
-                    # Restar uno a la cantidad si es mayor que 1
                     if cantidad_actual > 1:
                         self.__vista_ticket.tabla2.setItem(fila, 1, QTableWidgetItem(str(cantidad_actual - 1)))
-                    # Si la cantidad es 1, eliminar la fila
                     else:
                         self.__vista_ticket.tabla2.removeRow(fila)
-                    
-                    # Aumentar la cantidad en tabla 1
+
                     for fila1 in range(self.__vista_ticket.tabla1.rowCount()):
                         if self.__vista_ticket.tabla1.item(fila1, 0).text() == producto:
                             cantidad_disponible = int(self.__vista_ticket.tabla1.item(fila1, 2).text())
