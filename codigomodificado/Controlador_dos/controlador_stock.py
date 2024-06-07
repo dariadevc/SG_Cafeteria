4dba86dfb4062152414ccf359fd817de06826310
Daria Sauco <dsauco36@gmail.com>
2024-06-07 12:50:16 -0300

    07-06-24 [Visual]

--- codigomodificado/Controlador_dos/controlador_stock.py
+++ codigomodificado/Controlador_dos/controlador_stock.py
@@ -5,65 +5,103 @@ from Controlador_dos.controlador_agregar_producto import ControladorAgregarProdu
 from Controlador_dos.controlador_eliminar_producto import ControladorEliminarProducto
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import *
+
+
 class ControladorStock:
-    
+
     def __init__(self):
         self.__ventana_stock = VentanaStock(self)
         self.__producto_dao = ProductoDAO()
         self.__ventana_stock.show()
         self.cargar_productos()
 
-    
-    def cargar_productos (self):
+    def cargar_productos(self):
         self.__ventana_stock.limpiar_tabla()
         productos = self.__producto_dao.obtener_todos_productos()
+        productos_vigentes = [producto for producto in productos if producto[7]]
         print(productos)
-        self.__ventana_stock.tabla_productos.setRowCount(len(productos))
-        for fila, producto in enumerate(productos):
+        self.__ventana_stock.tabla_productos.setRowCount(len(productos_vigentes))
+        for fila, producto in enumerate(productos_vigentes):
             codigo = QTableWidgetItem(f"{producto[0]}")
             descripcion = QTableWidgetItem(f"{producto[1]}")
-            categoria = QTableWidgetItem(f"{producto[2]}")
+            # categoria = QTableWidgetItem(f"{producto[2]}")
+            categoria = QTableWidgetItem(self.categoria_to_nombre(producto[2]))
             fecha_mod = QTableWidgetItem(f"{producto[3]}")
             cantidad = QTableWidgetItem(f"{producto[4]}")
             stock_min = QTableWidgetItem(f"{producto[5]}")
             precio_unitario = QTableWidgetItem(f"{producto[6]}")
-            vigente = QTableWidgetItem(f"{producto[7]}")
-            causa = QTableWidgetItem(f"{producto[8]}")
-            
-            self.__ventana_stock.tabla_productos.setItem(fila,0,codigo)
-            self.__ventana_stock.tabla_productos.setItem(fila,1,descripcion)
-            self.__ventana_stock.tabla_productos.setItem(fila,2,categoria)
-            self.__ventana_stock.tabla_productos.setItem(fila,3,fecha_mod)
-            self.__ventana_stock.tabla_productos.setItem(fila,4,cantidad)
-            self.__ventana_stock.tabla_productos.setItem(fila,5,stock_min)
-            self.__ventana_stock.tabla_productos.setItem(fila,6,precio_unitario)
-            self.__ventana_stock.tabla_productos.setItem(fila,7,vigente)
-            self.__ventana_stock.tabla_productos.setItem(fila,8,causa)
-    
+            # vigente = QTableWidgetItem(f"{producto[7]}")
+            # causa = QTableWidgetItem(f"{producto[8]}")
+
+            self.__ventana_stock.tabla_productos.setItem(fila, 0, codigo)
+            self.__ventana_stock.tabla_productos.setItem(fila, 1, descripcion)
+            self.__ventana_stock.tabla_productos.setItem(fila, 2, categoria)
+            self.__ventana_stock.tabla_productos.setItem(fila, 3, fecha_mod)
+            self.__ventana_stock.tabla_productos.setItem(fila, 4, cantidad)
+            self.__ventana_stock.tabla_productos.setItem(fila, 5, stock_min)
+            self.__ventana_stock.tabla_productos.setItem(fila, 6, precio_unitario)
+            # self.__ventana_stock.tabla_productos.setItem(fila,7,vigente)
+            # self.__ventana_stock.tabla_productos.setItem(fila,8,causa)
+
+    def categoria_to_nombre(self, categoria_codigo):
+        if categoria_codigo == "A":
+            return "Bebidas"
+        elif categoria_codigo == "B":
+            return "Alimentos"
+        elif categoria_codigo == "C":
+            return "Helados"
+        else:
+            return "Desconocido"
+
     def get_vista(self):
         return self.__ventana_stock
-    
-    def agregar_producto (self):
+
+    def agregar_producto(self):
+        self.__ventana_stock.actualizar_color_boton(
+            self.__ventana_stock.boton_agregar_producto
+        )
         self.__controlador_agregar_producto = ControladorAgregarProducto()
-    
-    def modificar_producto (self):
+
+    def modificar_producto(self):
+        self.__ventana_stock.actualizar_color_boton(
+            self.__ventana_stock.boton_modificar_producto
+        )
         try:
             producto_seleccionado = self.__ventana_stock.tabla_productos.selectedItems()
-            producto = self.__producto_dao.obtener_un_producto(producto_seleccionado[0].text())
-            self.__controlador_modificar_producto = ControladorModificarProducto(producto)
+            producto = self.__producto_dao.obtener_un_producto(
+                producto_seleccionado[0].text()
+            )
+            self.__controlador_modificar_producto = ControladorModificarProducto(
+                producto
+            )
         except IndexError:
-            self.__ventana_stock.boton_modificar_producto.setStyleSheet("background-color:gray")
-            QTimer.singleShot(1000,self.cambio_de_color)
-    
-    def eliminar_producto (self):
+            self.__ventana_stock.boton_modificar_producto.setStyleSheet(
+                "background-color:gray"
+            )
+            QTimer.singleShot(1000, self.cambio_de_color)
+
+    def eliminar_producto(self):
+        self.__ventana_stock.actualizar_color_boton(
+            self.__ventana_stock.boton_eliminar_producto
+        )
         try:
             producto_seleccionado = self.__ventana_stock.tabla_productos.selectedItems()
-            producto_a_eliminar = self.__producto_dao.obtener_un_producto(producto_seleccionado[0].text())
-            self.__controlador_eliminar_producto = ControladorEliminarProducto(producto_a_eliminar[0])
+            producto_a_eliminar = self.__producto_dao.obtener_un_producto(
+                producto_seleccionado[0].text()
+            )
+            self.__controlador_eliminar_producto = ControladorEliminarProducto(
+                producto_a_eliminar[0]
+            )
         except IndexError:
-            self.__ventana_stock.boton_eliminar_producto.setStyleSheet("background-color:gray")
-            QTimer.singleShot(1000,self.cambio_de_color)
-    
-    def cambio_de_color (self):
-        self.__ventana_stock.boton_eliminar_producto.setStyleSheet("background-color: lightblue;")
-        self.__ventana_stock.boton_modificar_producto.setStyleSheet("background-color: lightblue;")
+            self.__ventana_stock.boton_eliminar_producto.setStyleSheet(
+                "background-color:gray"
+            )
+            QTimer.singleShot(1000, self.cambio_de_color)
+
+    # def cambio_de_color(self):
+    #     self.__ventana_stock.boton_eliminar_producto.setStyleSheet(
+    #         "background-color: lightblue;"
+    #     )
+    #     self.__ventana_stock.boton_modificar_producto.setStyleSheet(
+    #         "background-color: lightblue;"
+    #     )
--- codigomodificado/Visual_dos/ventana_stock.py
+++ codigomodificado/Visual_dos/ventana_stock.py
@@ -2,63 +2,100 @@ from PyQt6.QtWidgets import *
 from PyQt6.QtCore import *
 from PyQt6.QtGui import *
 
-class VentanaStock (QWidget):
-    
-    def __init__(self,controlador):
+
+class VentanaStock(QWidget):
+
+    def __init__(self, controlador):
         super().__init__()
         self.layout_ppal = QHBoxLayout()
-        
+
         ###################################################
         self.tabla_productos = QTableWidget()
-        self.tabla_productos.setColumnCount(9)
-        self.tabla_productos.setFixedSize(550,450)
-        self.tabla_productos.setColumnWidth(0,20)
-        self.tabla_productos.setColumnWidth(1,100)##descripcion
-        self.tabla_productos.setColumnWidth(2,80)##categoria
-        self.tabla_productos.setColumnWidth(3,60)##fecha
-        self.tabla_productos.setColumnWidth(4,70)##cantidad
-        self.tabla_productos.setColumnWidth(5,50)##stockminimo
-        self.tabla_productos.setColumnWidth(6,50)##baja
-        self.tabla_productos.setColumnWidth(7,95)
-        
-        self.tabla_productos.setHorizontalHeaderLabels(["cod","Descripcion","Cat","FechaMod","Cantidad","StockMin","Precio","Baja","Causa"])
+        self.tabla_productos.setColumnCount(7)
+        self.tabla_productos.setFixedSize(550, 450)
+        self.tabla_productos.setColumnWidth(0, 20)
+        self.tabla_productos.setColumnWidth(1, 100)  ##descripcion
+        self.tabla_productos.setColumnWidth(2, 80)  ##categoria
+        self.tabla_productos.setColumnWidth(3, 80)  ##fecha
+        self.tabla_productos.setColumnWidth(4, 70)  ##cantidad
+        self.tabla_productos.setColumnWidth(5, 60)  ##stockminimo
+        # self.tabla_productos.setColumnWidth(6,50)##vigencia
+        # self.tabla_productos.setColumnWidth(7,95)
+
+        # self.tabla_productos.setHorizontalHeaderLabels(["cod","Descripcion","Cat","FechaMod","Cantidad","StockMin","Precio","Baja","Causa"])
+        self.tabla_productos.setHorizontalHeaderLabels(
+            [
+                "Cod",
+                "Descripcion",
+                "Categoría",
+                "FechaMod",
+                "Cantidad",
+                "StockMin",
+                "Precio",
+            ]
+        )
         self.tabla_productos.verticalHeader().setVisible(False)
-        self.tabla_productos.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
+        self.tabla_productos.setEditTriggers(
+            QAbstractItemView.EditTrigger.NoEditTriggers
+        )
+        self.tabla_productos.setStyleSheet("background-color: white;")
+        self.tabla_productos.horizontalHeader().setStyleSheet(
+            "background-color: lightblue; font-weight: bold;"
+        )
         self.tabla_productos.cellClicked.connect(self.selecciono_fila)
         ###################################################
         self.layout_botonera = QVBoxLayout()
-        
-        icon = QPixmap("C://Users//Alan//Desktop//clon//codigomodificado//Visual_dos//imagenes//refrescar3.jpg").scaled(25,25)
-        
+
+        icon = QPixmap(
+            "C://Users//Alan//Desktop//clon//codigomodificado//Visual_dos//imagenes//refrescar3.jpg"
+        ).scaled(25, 25)
+
         self.boton_refrescar_tabla = QPushButton()
-        self.boton_refrescar_tabla.setFixedSize(80,30)
+        self.boton_refrescar_tabla.setStyleSheet("background-color: lightblue;")
+        self.boton_refrescar_tabla.setFixedSize(80, 30)
         self.boton_refrescar_tabla.setIcon(QIcon(icon))
         self.boton_refrescar_tabla.setIconSize(icon.size())
         self.boton_agregar_producto = QPushButton("AGREGAR PRODUCTO")
-        self.boton_agregar_producto.setFixedSize(120,70)
+        self.boton_agregar_producto.setStyleSheet("background-color: lightblue;")
+        self.boton_agregar_producto.setFixedSize(120, 70)
         self.boton_modificar_producto = QPushButton("MODIFICAR PRODUCTO")
-        self.boton_modificar_producto.setFixedSize(120,70)
+        self.boton_modificar_producto.setStyleSheet("background-color: lightblue;")
+        self.boton_modificar_producto.setFixedSize(120, 70)
         self.boton_eliminar_producto = QPushButton("ELIMINAR PRODUCTO")
-        self.boton_eliminar_producto.setFixedSize(120,70)
-        
+        self.boton_eliminar_producto.setStyleSheet("background-color: lightblue;")
+        self.boton_eliminar_producto.setFixedSize(120, 70)
+
         self.boton_agregar_producto.clicked.connect(controlador.agregar_producto)
         self.boton_modificar_producto.clicked.connect(controlador.modificar_producto)
         self.boton_eliminar_producto.clicked.connect(controlador.eliminar_producto)
         self.boton_refrescar_tabla.clicked.connect(controlador.cargar_productos)
-        
+
         self.layout_botonera.addWidget(self.boton_refrescar_tabla)
         self.layout_botonera.addWidget(self.boton_agregar_producto)
         self.layout_botonera.addWidget(self.boton_modificar_producto)
         self.layout_botonera.addWidget(self.boton_eliminar_producto)
-        self.layout_botonera.setContentsMargins(10,0,0,10)
+        self.layout_botonera.setContentsMargins(10, 0, 0, 10)
 
         self.layout_ppal.addWidget(self.tabla_productos)
         self.layout_ppal.addLayout(self.layout_botonera)
-        
+
         self.setLayout(self.layout_ppal)
-    
-    def selecciono_fila (self):
+
+    def selecciono_fila(self):
         self.tabla_productos.selectRow(self.tabla_productos.currentRow())
-    
-    def limpiar_tabla (self):
+
+    def limpiar_tabla(self):
         self.tabla_productos.clearContents()
+
+    def actualizar_color_boton(self, boton_seleccionado):
+        botones = [
+            self.boton_refrescar_tabla,
+            self.boton_agregar_producto,
+            self.boton_modificar_producto,
+            self.boton_eliminar_producto,
+        ]
+        for boton in botones:
+            if boton == boton_seleccionado:
+                boton.setStyleSheet("background-color: lightgray;")
+            else:
+                boton.setStyleSheet("background-color: lightblue;")
